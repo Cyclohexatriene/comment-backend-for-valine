479a687edcd16a72fe5b369384d9bc2130a50f7b
Cyclohexatriene <3200104203@zju.edu.cn>
2026-01-17 17:21:17 +0800

    1

--- src/main/resources/sql.sql
+++ src/main/resources/sql.sql
@@ -1,25 +1,25 @@
 CREATE TABLE comment
 (
     id          BIGINT(20)  AUTO_INCREMENT COMMENT '自增主键',
-    object_id   VARCHAR(32)  NOT NULL DEFAULT '' COMMENT '原始对象ID',
-    nick        VARCHAR(100) NOT NULL DEFAULT 'ANONYMOUS' COMMENT '昵称',
-    ip          VARCHAR(45)           DEFAULT '' COMMENT 'IP地址（支持IPv6）',
-    mail        VARCHAR(100)          DEFAULT '' COMMENT '邮箱',
-    link        VARCHAR(100)          DEFAULT '' COMMENT '链接',
-    url         VARCHAR(100)          DEFAULT '' COMMENT '页面URL',
-    comment     TEXT         NOT NULL COMMENT '评论内容（HTML格式）',
-    qq_avatar   VARCHAR(100)          DEFAULT '' COMMENT 'QQ头像链接',
-    ua          VARCHAR(1000)         DEFAULT '' COMMENT '用户代理字符串',
-    acl         VARCHAR(500)          DEFAULT '' COMMENT '访问控制列表（JSON字符串）',
-    inserted_at VARCHAR(100) NOT NULL DEFAULT '' COMMENT '插入时间（JSON字符串）',
+    object_id   VARCHAR(32)   NOT NULL DEFAULT '' COMMENT '原始对象ID',
+    nick        VARCHAR(100)  NOT NULL DEFAULT 'ANONYMOUS' COMMENT '昵称',
+    ip          VARCHAR(45)            DEFAULT '' COMMENT 'IP地址（支持IPv6）',
+    mail        VARCHAR(100)           DEFAULT '' COMMENT '邮箱',
+    link        VARCHAR(100)           DEFAULT '' COMMENT '链接',
+    url         VARCHAR(100)           DEFAULT '' COMMENT '页面URL',
+    comment     VARCHAR(2048) NOT NULL COMMENT '评论内容（HTML格式）',
+    qq_avatar   VARCHAR(100)           DEFAULT '' COMMENT 'QQ头像链接',
+    ua          VARCHAR(1000)          DEFAULT '' COMMENT '用户代理字符串',
+    acl         VARCHAR(500)           DEFAULT '' COMMENT '访问控制列表（JSON字符串）',
+    inserted_at VARCHAR(100)  NOT NULL DEFAULT '' COMMENT '插入时间（JSON字符串）',
 
     -- 关联关系字段
-    pid         VARCHAR(32)           DEFAULT '' COMMENT '父评论object_id',
-    rid         VARCHAR(32)           DEFAULT '' COMMENT '根评论object_id',
+    pid         VARCHAR(32)            DEFAULT '' COMMENT '父评论object_id',
+    rid         VARCHAR(32)            DEFAULT '' COMMENT '根评论object_id',
 
     -- 时间字段
-    created_at  DATETIME     NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-    updated_at  DATETIME     NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+    created_at  DATETIME      NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+    updated_at  DATETIME      NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
 
     -- 索引
     PRIMARY KEY (`Id`),
